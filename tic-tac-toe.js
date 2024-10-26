8c2f0397d79c6138ee3e44802e5124c0aefa3780
Kaydeen Tucker <shinnakbin@gmail.com>
2024-10-25 21:12:52 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,10 +8,23 @@ document.addEventListener("DOMContentLoaded", function() {
     squares.forEach((square, index) => {
         square.classList.add("square");
 
+        square.addEventListener("mouseover", function(){
+            if (!boardState[index]) {
+                square.classList.add("hover");  
+            }
+        });
+
+        //mouseout effect
+        square.addEventListener("mouseout", function(){
+            square.classList.remove("hover");
+        });
+
         square.addEventListener("click", function(){
             if (!boardState[index]) {
                 boardState[index] = currentPlayer;
                 square.textContent = currentPlayer;
+
+                square.classList.add(currentPlayer);
                 
                 currentPlayer = currentPlayer === "X"? "O" : "X";
             }
